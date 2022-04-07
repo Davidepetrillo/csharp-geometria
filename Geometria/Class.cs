9e895e6f71900a393a2ffce325288814bb6b5256
davidepetrillo <davide96petrillo@gmail.com>
2022-04-07 15:51:13 +0200

    Add project files.

---
+++ Geometria/Class.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    internal class Rettangolo
+    {
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+
+        public Rettangolo (int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+
+        public int CalcolaArea(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+            int areaRettangolo = baseRettangolo * altezzaRettangolo;
+            return areaRettangolo;
+        }
+
+        public int CalcolaPerimetro(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+            int perimetroRettangolo = 2 * (baseRettangolo + altezzaRettangolo);
+            return perimetroRettangolo;
+        }
+
+
+
+    }
+}
---
+++ Geometria/Program.cs
@@ -0,0 +1,5 @@
+﻿using Geometria;
+Rettangolo IlMioRettangolo = new Rettangolo(4, 2);
+
+Console.WriteLine($"L'area del mio rettangolo è {IlMioRettangolo.CalcolaArea(4, 2)}");
+Console.WriteLine($"Il perimetro del mio rettangolo è {IlMioRettangolo.CalcolaPerimetro(4, 2)}");
