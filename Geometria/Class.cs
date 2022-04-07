cd051d36d0f8371da14654fc74e7123b5779fad3
davidepetrillo <davide96petrillo@gmail.com>
2022-04-07 17:08:32 +0200

    commit 2 - instanziati altri rettangoli

--- Geometria/Class.cs
+++ Geometria/Class.cs
@@ -32,8 +32,107 @@ namespace Geometria
             int perimetroRettangolo = 2 * (baseRettangolo + altezzaRettangolo);
             return perimetroRettangolo;
         }
+    }
 
 
+
+    // ---------- Secondo Rettangolo ----------
+
+
+
+    internal class RettangoloDue
+    {
+        public int baseRettangoloDue;
+        public int altezzaRettangoloDue;
+
+        public RettangoloDue(int baseRettangoloDue, int altezzaRettangoloDue)
+        {
+            this.baseRettangoloDue = baseRettangoloDue;
+            this.altezzaRettangoloDue = altezzaRettangoloDue;
+        }
+
+        public int CalcolaAreaDue(int baseRettangoloDue, int altezzaRettangoloDue)
+        {
+            this.baseRettangoloDue = baseRettangoloDue;
+            this.altezzaRettangoloDue = altezzaRettangoloDue;
+            int areaRettangoloDue = baseRettangoloDue * altezzaRettangoloDue;
+            return areaRettangoloDue;
+        }
+
+        public int CalcolaPerimetroDue(int baseRettangoloDue, int altezzaRettangoloDue)
+        {
+            this.baseRettangoloDue = baseRettangoloDue;
+            this.altezzaRettangoloDue = altezzaRettangoloDue;
+            int perimetroRettangoloDue = 2 * (baseRettangoloDue + altezzaRettangoloDue);
+            return perimetroRettangoloDue;
+        }
+    }
+
+
+
+    // ----------- Terzo Rettangolo -----------
+
+
+
+    internal class RettangoloTre
+    {
+        public int baseRettangoloTre;
+        public int altezzaRettangoloTre;
+
+        public RettangoloTre(int baseRettangoloTre, int altezzaRettangoloTre)
+        {
+            this.baseRettangoloTre = baseRettangoloTre;
+            this.altezzaRettangoloTre = altezzaRettangoloTre;
+        }
+
+        public int CalcolaAreaTre(int baseRettangoloTre, int altezzaRettangoloTre)
+        {
+            this.baseRettangoloTre = baseRettangoloTre;
+            this.altezzaRettangoloTre = altezzaRettangoloTre;
+            int areaRettangoloTre = baseRettangoloTre * altezzaRettangoloTre;
+            return areaRettangoloTre;
+        }
+
+        public int CalcolaPerimetroTre(int baseRettangoloTre, int altezzaRettangoloTre)
+        {
+            this.baseRettangoloTre = baseRettangoloTre;
+            this.altezzaRettangoloTre = altezzaRettangoloTre;
+            int perimetroRettangoloTre = 2 * (baseRettangoloTre + altezzaRettangoloTre);
+            return perimetroRettangoloTre;
+        }
 
     }
+
+        // ---------- Quarto Rettangolo ----------
+
+
+
+        internal class RettangoloQuattro
+        {
+            public int baseRettangoloQuattro;
+            public int altezzaRettangoloQuattro;
+
+            public RettangoloQuattro(int baseRettangoloQuattro, int altezzaRettangoloQuattro)
+            {
+                this.baseRettangoloQuattro = baseRettangoloQuattro;
+                this.altezzaRettangoloQuattro = altezzaRettangoloQuattro;
+            }
+
+            public int CalcolaAreaQuattro(int baseRettangoloQuattro, int altezzaRettangoloQuattro)
+            {
+                this.baseRettangoloQuattro = baseRettangoloQuattro;
+                this.altezzaRettangoloQuattro = altezzaRettangoloQuattro;
+                int areaRettangoloQuattro = baseRettangoloQuattro * altezzaRettangoloQuattro;
+                return areaRettangoloQuattro;
+            }
+
+            public int CalcolaPerimetroQuattro(int baseRettangoloQuattro, int altezzaRettangoloQuattro)
+            {
+                this.baseRettangoloQuattro = baseRettangoloQuattro;
+                this.altezzaRettangoloQuattro = altezzaRettangoloQuattro;
+                int perimetroRettangoloQuattro = 2 * (baseRettangoloQuattro + altezzaRettangoloQuattro);
+                return perimetroRettangoloQuattro;
+            }
+        }
+    
 }
--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,10 +1,60 @@
 ﻿using Geometria;
+
+
+// ---------- Variabili Globali -----------
+
+
 Console.Write("Inserisci un numero che faccia da base per il tuo rettangolo: ");
 int baseSceltaDalUtente = int.Parse(Console.ReadLine());
 Console.WriteLine();
 Console.Write("Inserisci un numero che faccia da base per il tuo rettangolo: ");
 int altezzaSceltaDallUtente = int.Parse(Console.ReadLine());
+
+
+// ----------- Programma -----------
+
+
 Rettangolo IlMioRettangolo = new Rettangolo(baseSceltaDalUtente, altezzaSceltaDallUtente);
+RettangoloDue IlMioRettangoloDue = new RettangoloDue(8, 4);
+RettangoloTre IlMioRettangoloTre = new RettangoloTre(10, 5);
+RettangoloQuattro IlMioRettangoloQuattro = new RettangoloQuattro(12, 8);
+
 
 Console.WriteLine($"L'area del mio rettangolo è {IlMioRettangolo.CalcolaArea(baseSceltaDalUtente, altezzaSceltaDallUtente)}");
 Console.WriteLine($"Il perimetro del mio rettangolo è {IlMioRettangolo.CalcolaPerimetro(baseSceltaDalUtente, altezzaSceltaDallUtente)}");
+Console.WriteLine();
+Console.WriteLine();
+
+
+Console.WriteLine($"---------- Rettangolo1 ----------");
+Console.WriteLine($" Base : {IlMioRettangolo.baseRettangolo} cm");
+Console.WriteLine($" Altezza : {IlMioRettangolo.altezzaRettangolo} cm");
+Console.WriteLine($" Perimetro : {IlMioRettangolo.CalcolaArea(baseSceltaDalUtente, altezzaSceltaDallUtente)} cm");
+Console.WriteLine($" Area : {IlMioRettangolo.CalcolaPerimetro(baseSceltaDalUtente, altezzaSceltaDallUtente)} cm2");
+Console.WriteLine();
+
+
+Console.WriteLine($"---------- Rettangolo2 ----------");
+Console.WriteLine($" Base : {IlMioRettangoloDue.baseRettangoloDue} cm");
+Console.WriteLine($" Altezza : {IlMioRettangoloDue.altezzaRettangoloDue} cm");
+Console.WriteLine($" Perimetro : {IlMioRettangoloDue.CalcolaAreaDue(8, 4)} cm");
+Console.WriteLine($" Area : {IlMioRettangoloDue.CalcolaPerimetroDue(8, 4)} cm2");
+Console.WriteLine();
+
+
+Console.WriteLine($"---------- Rettangolo3 ----------");
+Console.WriteLine($" Base : {IlMioRettangoloTre.baseRettangoloTre} cm");
+Console.WriteLine($" Altezza : {IlMioRettangoloTre.altezzaRettangoloTre} cm");
+Console.WriteLine($" Perimetro : {IlMioRettangoloTre.CalcolaAreaTre(10, 5)} cm");
+Console.WriteLine($" Area : {IlMioRettangoloTre.CalcolaPerimetroTre(10, 5)} cm2");
+Console.WriteLine();
+
+Console.WriteLine($"---------- Rettangolo4 ----------");
+Console.WriteLine($" Base : {IlMioRettangoloQuattro.baseRettangoloQuattro} cm");
+Console.WriteLine($" Altezza : {IlMioRettangoloQuattro.altezzaRettangoloQuattro} cm");
+Console.WriteLine($" Perimetro : {IlMioRettangoloQuattro.CalcolaAreaQuattro(12, 8)} cm");
+Console.WriteLine($" Area : {IlMioRettangoloQuattro.CalcolaPerimetroQuattro(12, 8)} cm2");
+Console.WriteLine();
+
+
+
