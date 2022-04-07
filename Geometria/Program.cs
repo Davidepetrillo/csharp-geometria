5762c248e5eaa90a4a535217503c0be7f7f2321d
davidepetrillo <davide96petrillo@gmail.com>
2022-04-07 16:24:59 +0200

    commit 1 - aggiunto readLine

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,5 +1,10 @@
 ﻿using Geometria;
-Rettangolo IlMioRettangolo = new Rettangolo(4, 2);
+Console.Write("Inserisci un numero che faccia da base per il tuo rettangolo: ");
+int baseSceltaDalUtente = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Inserisci un numero che faccia da base per il tuo rettangolo: ");
+int altezzaSceltaDallUtente = int.Parse(Console.ReadLine());
+Rettangolo IlMioRettangolo = new Rettangolo(baseSceltaDalUtente, altezzaSceltaDallUtente);
 
-Console.WriteLine($"L'area del mio rettangolo è {IlMioRettangolo.CalcolaArea(4, 2)}");
-Console.WriteLine($"Il perimetro del mio rettangolo è {IlMioRettangolo.CalcolaPerimetro(4, 2)}");
+Console.WriteLine($"L'area del mio rettangolo è {IlMioRettangolo.CalcolaArea(baseSceltaDalUtente, altezzaSceltaDallUtente)}");
+Console.WriteLine($"Il perimetro del mio rettangolo è {IlMioRettangolo.CalcolaPerimetro(baseSceltaDalUtente, altezzaSceltaDallUtente)}");
